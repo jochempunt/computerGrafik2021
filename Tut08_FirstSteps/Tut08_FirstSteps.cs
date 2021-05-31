bbcd53011993bf163f210a11ba9d7c68c98b948f
Jochem Punt <jochem.punt@gmail.com>
2021-05-31 08:46:06 +0200

    aufgabe

--- Tut08_FirstSteps/Tut08_FirstSteps.cs
+++ Tut08_FirstSteps/Tut08_FirstSteps.cs
@@ -4,6 +4,7 @@ using Fusee.Engine.Common;
 using Fusee.Engine.Core;
 using Fusee.Engine.Core.Scene;
 using Fusee.Math.Core;
+using Fusee.Engine.Core.Effects;
 using Fusee.Serialization;
 using Fusee.Xene;
 using static Fusee.Engine.Core.Input;
@@ -25,19 +26,31 @@ namespace FuseeApp
         private Transform _cubeTransform1;
 
         private Transform _cubeTransform2;
+
+        private Transform _cubeTransform3;
+
+        private Transform _cubeTransform4;
     
+        private DefaultSurfaceEffect _cubeshader;
+
+        private DefaultSurfaceEffect _cubeshader2;
+
+        private Mesh _cubeMesh;
+
+
+        private SceneNode node;
 
         public override void Init()
         {
             // Set the clear color for the backbuffer to "greenery"
-            RC.ClearColor = (float4) new ColorUint(255,86,128,255);
+            RC.ClearColor =  (float4) (ColorUint.Black);
             // Create a scene with a cube
         // The three components: one Transform, one ShaderEffect (blue material) and the Mesh
-            _cubeTransform1 = new Transform {
+           /* _cubeTransform1 = new Transform {
                 Translation = new float3(-10, 0, 40),
                 Rotation = new float3(0,0.3f,0),
                 };
-            var cubeShader = MakeEffect.FromDiffuseSpecular((float4)ColorUint.Cyan, float4.Zero);
+            _cubeshader = MakeEffect.FromDiffuseSpecular((float4)ColorUint.Cyan, float4.Zero);
             var cubeMesh = SimpleMeshes.CreateCuboid(new float3(10, 10, 10));
         
 
@@ -51,20 +64,94 @@ namespace FuseeApp
 
             var Cubenode2 = new SceneNode();
             Cubenode2.Components.Add(_cubeTransform2);
-            Cubenode2.Components.Add(cubeShader);
+            Cubenode2.Components.Add(_cubeshader);
             Cubenode2.Components.Add(cubeMesh);
             
             var cubeNode = new SceneNode(); //Hauptnode
             cubeNode.Components.Add(_cubeTransform1);// kinderKomponenten werden gesetzt
-            cubeNode.Components.Add(cubeShader);
+            cubeNode.Components.Add(_cubeshader);
             cubeNode.Components.Add(cubeMesh);
 
         // Create the scene containing the cube as the only object
             _scene = new SceneContainer();
             _scene.Children.Add(cubeNode);
-            _scene.Children.Add(Cubenode2);                                // in childrenliste der szene einreihen
-
+           _scene.Children.Add(Cubenode2);                                // in childrenliste der szene einreihen
+*/
         // Create a scene renderer holding the scene above
+            
+            
+            _cubeTransform1 = new Transform {
+                Translation = new float3(-10, 0, 70),
+                Rotation = new float3(0,0.3f,0),
+                };
+            _cubeshader = MakeEffect.FromDiffuseSpecular((float4)ColorUint.Cyan, float4.Zero);
+            _cubeshader2 = MakeEffect.FromDiffuseSpecular((float4)ColorUint.Cyan, float4.Zero);
+            
+            _cubeTransform2 = new Transform{
+                Translation = new float3(10,0,70),
+                Rotation = new float3(0,0.3f,0),
+            };
+
+            _cubeTransform3 = new Transform{
+                Translation = new float3(0,0,70),
+                Rotation = new float3(0,0.3f,0),
+            };
+
+            _cubeTransform4 = new Transform {
+                Translation = new float3(0,0,70),
+                Rotation = new float3(0,0.3f,0),
+            };
+
+            _cubeMesh = SimpleMeshes.CreateCuboid(new float3(6,6,6));
+
+
+            node = new SceneNode{
+                Components =
+                {
+                     _cubeTransform4,
+                     _cubeshader2,
+                     _cubeMesh,
+                    
+                }
+            };
+
+            _scene = new SceneContainer
+            { 
+                Children = 
+                {
+                    new SceneNode 
+                    {
+                        Components = 
+                        {
+                            _cubeTransform1,
+                            _cubeshader,
+                            SimpleMeshes.CreateCuboid(new float3(3,3,3))
+                        }
+                    },
+                    new SceneNode
+                    {
+                        Components = 
+                        {
+                            _cubeTransform2,
+                            _cubeshader,
+                            SimpleMeshes.CreateCuboid(new float3(3,3,3))
+                        }
+                    },
+                    new SceneNode
+                    {
+                        Components =
+                        {
+                            _cubeTransform3,
+                            _cubeshader,
+                            SimpleMeshes.CreateCuboid(new float3(3,3,3))
+                        }
+                    },
+                    node
+                }
+
+            };
+            
+
             _sceneRenderer = new SceneRendererForward(_scene);//renderer der einzelne pixel einfärben kann 
         }
 
@@ -77,8 +164,40 @@ namespace FuseeApp
             RC.Clear(ClearFlags.Color | ClearFlags.Depth); // depth buffer, tiefe
 
 
-            _cubeTransform1.Rotation = _cubeTransform1.Rotation + new float3(0,0.005f,0);
-            _cubeTransform2.Rotation = _cubeTransform2.Rotation - new float3(0,0.005f,0);
+            _cubeTransform1.Rotation = _cubeTransform1.Rotation + new float3(0,90 *(M.Pi /180.0f) * Time.DeltaTime,0); // umrechnung von pro fram in pro sekunde
+            _cubeTransform1.Translation.x = 20* M.Sin(2* Time.TimeSinceStart);
+            _cubeTransform1.Translation.y = 20*M.Cos(2* Time.TimeSinceStart);
+            _cubeTransform2.Rotation = _cubeTransform2.Rotation - new float3(0,90 *(M.Pi /180.0f) * Time.DeltaTime ,0);
+            Diagnostics.Debug("Keyboard <-> Axis: " + Keyboard.LeftRightAxis);
+           
+
+           _cubeTransform2.Translation.x = 20* M.Sin(2* Time.TimeSinceStart);
+           _cubeTransform2.Translation.z = 20*M.Cos(2* Time.TimeSinceStart)+70;
+
+
+           _cubeTransform3.Rotation = _cubeTransform2.Rotation;
+            _cubeTransform4.Rotation =  _cubeTransform2.Rotation;
+
+
+           _cubeTransform3.Translation.y =  20*M.Sin(2* Time.TimeSinceStart);
+           _cubeTransform3.Translation.z =  20*M.Cos(-2* Time.TimeSinceStart)+70;
+           
+           _cubeTransform4.Scale.x = (0.3f * M.Sin(2*Time.TimeSinceStart)) + 1;
+           _cubeTransform4.Scale.y = (0.3f * M.Sin(2*Time.TimeSinceStart)) + 1;
+           _cubeTransform4.Scale.z = (0.3f * M.Sin(2*Time.TimeSinceStart)) +1+ (0.5f * Keyboard.LeftRightAxis);
+           
+           
+           if( Mouse.LeftButton ){
+                 _cubeshader2.SurfaceInput.Albedo = new float4(0.5f + 0.5f *M.Sin(2*Time.TimeSinceStart),0,0.5f,1);
+           }else{
+               _cubeshader.SurfaceInput.Albedo =  new float4(0.5f +0.5f * M.Cos(2 * Time.TimeSinceStart), 0.5f +0.5f * M.Sin(4 * Time.TimeSinceStart),0.5f +0.5f * M.Cos(2 * Time.TimeSinceStart),1);
+           }
+
+         
+           
+           
+           
+           
             _sceneRenderer.Render(RC);
 
             //hier spielt die musik
